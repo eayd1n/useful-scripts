7045f44c1d378be5a9e619483391fd0b89e91ffa
Ekrem Aydin <ekrem.aydin@aptiv.com>
2024-03-05 09:10:28 +0100

    Add script to update git repos + shellcheck to CI

---
+++ update_gitrepos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+### This script pulls the current state from main branch for each git repository located in specific directory.
+
+declare -r REPOS_DIR="/home/ekrem/Documents/gitrepos"
+declare -r GREEN='\033[0;32m'
+declare -r NOCOLOR='\033[0m'
+
+for dir in "${REPOS_DIR}"/*; do
+  if [ -d "${dir}" ]; then
+    repo_name="${GREEN}$(basename "${dir}")${NOCOLOR}"
+
+    echo ""
+    echo ""
+    echo -e "================================================================================> Updating ${repo_name}"
+    echo ""
+    echo ""
+
+    cd "${dir}" || exit
+    git checkout main 2> /dev/null && git pull --rebase
+  fi
+done
